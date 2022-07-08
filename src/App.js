cb295747a7baace1be455e6abf32c93673e432cc
Umangcodesnow <umangcodes@gmail.com>
2022-07-08 13:46:55 +0530

    Updating Dom

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=e314914c";
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
